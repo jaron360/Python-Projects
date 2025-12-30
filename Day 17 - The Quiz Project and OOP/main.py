6f3c68e70d987da1a57a2cd818e4be4e90a96f44
Jaron <jdbedwards5@gmail.com>
2025-12-29 20:18:55 -0700

    Create main.py

---
+++ Day 17 - The Quiz Project and OOP/main.py
@@ -0,0 +1,21 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz_brain = QuizBrain(question_bank)
+quiz_brain.next_question()
+
+while quiz_brain.still_has_question() == True:
+    quiz_brain.next_question()
+
+print("You have completed the quiz")
+print(f"Your final score is {quiz_brain.score}/{quiz_brain.question_number}")
